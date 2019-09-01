3b105d177a1a48b9cf47e3783d308a2ea1724c74
guzhongren <guzhongren@live.cn>
2019-09-01 22:45:00 +0800

    test(Result): 初步使用Result  失败

--- src/a/b/c/d.rs
+++ src/a/b/c/d.rs
@@ -1,4 +1,13 @@
 
 pub fn print_ddd () {
   println!("i'a ddd");
-} 
+}
+
+pub fn find(haystack: &str, needle: char) -> Option<usize> {
+  for (offset, c ) in haystack.char_indices() {
+    if c == needle {
+      return Some(offset);
+    }
+  }
+  None
+}
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,23 @@
 // mod matchs;
 // mod traits;
 // mod genric;
-mod borrow;
-mod a;
-use a::b::c::d;
+// mod borrow;
+// mod a;
+// use a::b::c::d;
+
+mod file;
+
 
 fn main() {
-    borrow::run();
+    // borrow::run();
     // cli::run();
-    d::print_ddd();
+    // d::print_ddd();
+    // let file_name = "foobar.rs";
+    // match d::find(file_name, '.') {
+    //     None => println!("No file extension found."),
+    //     Some(i) => println!("File extension is {}",&file_name[i+1..]),
+    // }
+    file::file::read_file();
+    
     println!("hello");
 }
