e6d6aea79912a5aa09df6a142fa8ff904a460311
guzhongren <guzhongren@live.cn>
2019-09-28 22:47:47 +0800

    feat(db): 练习db的链接问题

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
-// mod basic;
-// mod enums;
-// mod cli;
-// mod hash;
-// mod matchs;
-// mod traits;
-// mod genric;
-// mod borrow;
-// mod a;
-// use a::b::c::d;
-// mod file;
-// mod std;
-mod impls;
+extern crate postgres;
+
+use postgres::{Connection, TlsMode};
+
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
 fn main() {
-    // borrow::run();
-    // cli::run();
-    // d::print_ddd();
-    // let file_name = "foobar.rs";
-    // match d::find(file_name, '.') {
-    //     None => println!("No file extension found."),
-    //     Some(i) => println!("File extension is {}",&file_name[i+1..]),
-    // }
-    // file::file::read_file();
-    // std::run();
-    impls::run();
-    println!("hello");
+    println!("Hello, world!");
+    let url = "postgresql://postgres:000000@localhost:5432/rust";
+    let conn = Connection::connect(url, TlsMode::None).unwrap();
+    // println!("{:?}", conn);
+    let me = Person {
+        id: 0,
+        name: String::from("guzhongren"),
+        data: None
+    };
+
+    let rows_updated = conn.execute("create table person (
+        id              SERIAL PRIMARY KEY,
+        name            VARCHAR NOT NULL,
+        data            BYTEA
+    )", &[]).unwrap();
+    println!("{}", rows_updated);
+    let rows_updated = conn.execute("insert into person(name, data) values($1, $2)", &[&me.name, &me.data]).unwrap();
+    println!("{}", rows_updated );
+
+    let all = conn.query("select id, name, data from person", &[]).unwrap();
+    for row in &all {
+        let person = Person{
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2)
+        };
+        println!("Person: {}", person.name);
+    }
+    
 }
