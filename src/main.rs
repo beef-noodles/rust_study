77aa2e62a1e094367b3535f959c4a04dfbdfc552
guzhongren <guzhongren@live.cn>
2019-10-03 17:03:59 +0800

    pref(db): 优化

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod utils;
 mod modal;
 use utils::db::{get_client, init_db};
 use modal::blog::{Blog, insert as insert_blog, query_all as query_all_blog};
-
 pub fn main() {
     let mut client = get_client();
     init_db(&mut client);
