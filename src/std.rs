fc6b42267d54638987b94831fff5973f84f9d9ba
guzhongren <guzhongren@live.cn>
2020-04-16 22:47:50 +0800

    init

--- src/std.rs
+++
@@ -1,7 +0,0 @@
-pub fn run() -> i32 {
-  let mut input = String::new();
-  std::io::stdin().read_line(&mut input).expect("Failed to read line");
-  let num = input.trim().parse::<i32>().unwrap();
-  println!("您输入的数字是：{}", num);
-  num
-}
