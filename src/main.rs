89e0eaa70fe9790c9ea51ea8fffe85c757bcb807
guzhongren <guzhongren@live.cn>
2020-04-19 11:12:57 +0800

    feat(deref): channel 通道

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,40 @@
 //    x + 1
 
 
-use std::ops::Deref;
+// use std::ops::Deref;
+// 
+// 
+// struct MyBox<T>(T);
+// 
+// impl <T> MyBox<T>{
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+// impl <T> Deref for MyBox<T> {
+//     type Target = T;
+//     fn deref(&self) -> &T {
+//         &self.0
+//     }
+// }
+// 
+// fn main() {
+//     let x = 5;
+//     let y = MyBox::new(x);
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+//     println!("Hello World!");
+// }
 
-
-struct MyBox<T>(T);
-
-impl <T> MyBox<T>{
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-impl <T> Deref for MyBox<T> {
-    type Target = T;
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
+use std::thread;
+use std::sync::mpsc;
 
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-    println!("Hello World!");
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move|| {
+        let val = String::from("Hi");
+        tx.send(val).unwrap();
+    });
+    let received = rx.recv().unwrap();
+    println!("Got {}", received);
 }
