e04da56424176c316fc3f4b08c9b573ec8171738
guzhongren <guzhongren@live.cn>
2020-04-26 21:59:29 +0800

    feat(thread): 通道+ 多线程

--- src/main.rs
+++ src/main.rs
@@ -42,13 +42,23 @@
 
 use std::thread;
 use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
     thread::spawn(move|| {
-        let val = String::from("Hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
-    let received = rx.recv().unwrap();
-    println!("Got {}", received);
+    for r in rx {
+        println!("Got {}", r);
+    }
 }
