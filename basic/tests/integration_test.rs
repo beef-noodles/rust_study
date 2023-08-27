74fcb2127b35319c9425bc2c49445a92f51296ae
guzhongren <guzhongren@live.cn>
2023-08-27 22:13:22 +0800

    feat(workspace): use workspace

--- basic/tests/integration_test.rs
+++ basic/tests/integration_test.rs
@@ -1,14 +1,12 @@
-extern crate rust_study;
-
 #[test]
 fn it_works_in_tests() {
-    assert_eq!(5, rust_study::add_three(2));
+    assert_eq!(5, basic::add_three(2));
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_in_mod() {
-        assert_eq!(1000, rust_study::add_three(997));
+        assert_eq!(1000, basic::add_three(997));
     }
 }
--- workspace/src/main.rs
+++ workspace/src/main.rs
@@ -1,6 +1,25 @@
 extern crate basic;
+extern crate env_logger;
+extern crate log;
+
 use basic::enums;
 
+use env_logger::{Builder, Target};
+use log::{debug, error, info};
+
+fn init_log() {
+    let mut builder = Builder::from_default_env();
+    builder.target(Target::Stdout);
+    builder.init();
+}
+
 fn main() {
+    info!("Start to run with workspace");
+    init_log();
+
     enums::run();
+
+    info!("info log");
+    debug!("debug log");
+    error!("Failed to run");
 }
